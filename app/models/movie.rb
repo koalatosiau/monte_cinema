12de8878118356ed3a588dc026ea376da3d41218
Alicja Szynka <alicja.szynka@monterail.com>
2022-09-16 11:55:32 +0200

    Correct rubocop offences

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
   validates :title, presence: true
   validates :description, length: { maximum: 500 }
-  validates :runtime, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :runtime, presence: true, numericality: { greater_than: 0 }
 end
