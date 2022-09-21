2ed604c902ac797b6dc2ffb71f3351676eee45a0
Alicja Szynka <koalatosiau@gmail.com>
2022-09-21 11:45:48 +0200

    Update spec/factories/screenings.rb
    Co-authored-by: Lila Łomnicka <lilla.lomnicka@monterail.com>

--- spec/factories/screenings.rb
+++ spec/factories/screenings.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :screening do
-    hall_id { create(:hall).id }
-    movie_id { create(:movie).id }
+    hall
+    movie
     start { Faker::Time.forward(days: 3, period: :evening) }
   end
 end
+
