fad4b2ce05725eb3b04c33fd4d88a9de6e4a71fa
Alicja Szynka <alicja.szynka@monterail.com>
2022-09-22 17:28:18 +0200

    Add movie show view and controller method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: %i[show]
+
   def index
     @movies = Movie.all
   end
@@ -17,7 +19,13 @@ class MoviesController < ApplicationController
     end
   end
 
+  def show; end
+
   private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
 
   def movie_params
     params.require(:movie).permit(:title, :description, :runtime)
