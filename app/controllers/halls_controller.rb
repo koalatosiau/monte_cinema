cd60b7e5d6d1b0b78de246e4f836df8720906c64
Alicja Szynka <alicja.szynka@monterail.com>
2022-09-22 13:06:48 +0200

    Add halls new view and controller method

--- app/controllers/halls_controller.rb
+++ app/controllers/halls_controller.rb
@@ -2,4 +2,23 @@ class HallsController < ApplicationController
   def index
     @halls = Hall.all
   end
+
+  def new
+    @hall = Hall.new
+  end
+
+  def create
+    @hall = Hall.new(hall_params)
+
+    if @hall.save
+      redirect_to halls_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def hall_params
+    params.require(:hall).permit(:name, :capacity)
+  end
 end
