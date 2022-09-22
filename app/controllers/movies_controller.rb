38000b3a170cc6ce989e209089ef9d1c05b713e5
Alicja Szynka <alicja.szynka@monterail.com>
2022-09-22 17:24:23 +0200

    Add movie new view and controller method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,24 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :runtime)
+  end
 end
