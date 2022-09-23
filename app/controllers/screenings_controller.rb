2186655da3ef0312bdbdd3c4785ae8d874da0410
Alicja Szynka <alicja.szynka@monterail.com>
2022-09-23 11:06:45 +0200

    Add screening new view and controller method

--- app/controllers/screenings_controller.rb
+++ app/controllers/screenings_controller.rb
@@ -1,5 +1,35 @@
 class ScreeningsController < ApplicationController
+  before_action :set_hall, :set_movie, only: %i[new create]
+
   def index
     @screenings = Screening.all
+  end
+
+  def new
+    @screening = Screening.new
+  end
+
+  def create
+    @screening = Screening.new(screening_params)
+
+    if @screening.save
+      redirect_to screenings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_hall
+    @halls = Hall.all.order(:name)
+  end
+
+  def set_movie
+    @movies = Movie.all.order(:title)
+  end
+
+  def screening_params
+    params.require(:screening).permit(:start, :hall_id, :movie_id)
   end
 end
