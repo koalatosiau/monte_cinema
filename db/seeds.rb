ebe0d29929371b8187fff062b3ee278db07b4ed3
Alicja Szynka <alicja.szynka@monterail.com>
2022-09-27 10:57:07 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+hall_capacities = [10, 15, 20, 30, 35, 50, 100, 150]
+hall_capacities.each_with_index do |capacity, index|
+    Hall.find_or_create_by(name: Faker::Color.color_name.capitalize, capacity: capacity)
+end
+
+movie_runtimes = [72, 90, 99, 100, 120, 140, 175]
+movie_runtimes.each do |runtime, index|
+  Movie.find_or_create_by(title: Faker::Movie.title, description: Faker::Lorem.paragraph, runtime: runtime)
+end
