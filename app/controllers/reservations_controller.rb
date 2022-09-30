27647ee15765d7241f202d79c28909166717eb9b
Alicja Szynka <koalatosiau@gmail.com>
2022-09-30 12:33:30 +0200

    Update app/controllers/reservations_controller.rb
    Co-authored-by: Mateusz Pietrzyk <protoplaste@gmail.com>

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,7 +3,7 @@ class ReservationsController < ApplicationController
   before_action :set_screenings, only: %i[new edit]
 
   def index
-    @reservations = Reservation.all.includes(screening: :movie)
+    @reservations = Reservation.includes(screening: :movie)
   end
 
   def new
