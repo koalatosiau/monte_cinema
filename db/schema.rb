e606bf2d31e3fb48ea7d46ff1ab30083c20364c8
Alicja Szynka <alicja.szynka@monterail.com>
2022-10-03 20:51:55 +0200

    Add emails to reservations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_02_181250) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_03_172937) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_02_181250) do
     t.bigint "screening_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
     t.index ["screening_id"], name: "index_reservations_on_screening_id"
     t.check_constraint "quantity >= 1", name: "quantity_check"
   end
