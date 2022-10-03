eda6af6070c2b02d9bc3cef513e7d71b3b18210a
Alicja Szynka <alicja.szynka@monterail.com>
2022-10-03 15:44:59 +0200

    Add styling to hall, movie, screening and reservation CRUD views

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,4 +4,6 @@ class Reservation < ApplicationRecord
 
   validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 1 }
   validates :status, presence: true
+
+  delegate :movie_title_with_start, to: :screening
 end
--- app/models/screening.rb
+++ app/models/screening.rb
@@ -5,7 +5,7 @@ class Screening < ApplicationRecord
 
   validates :start, presence: true
 
-  def movie_tite_with_start
+  def movie_title_with_start
     "#{movie.title}, #{start.strftime('%d %b %Y, %H:%M')}"
   end
 end
