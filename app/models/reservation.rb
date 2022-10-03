00323385b8f4f295ea766007a8fd47e11b0367f7
Alicja Szynka <alicja.szynka@monterail.com>
2022-10-03 21:13:04 +0200

    Update reservation model validations, factory and rspec

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,6 +4,7 @@ class Reservation < ApplicationRecord
 
   validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 1 }
   validates :status, presence: true
+  validates :email, presence: true
 
   delegate :movie_title_with_start, to: :screening
 end
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -5,5 +5,6 @@ RSpec.describe Reservation, type: :model do
     it { is_expected.to(validate_presence_of(:quantity)) }
     it { is_expected.to(validate_numericality_of(:quantity).is_greater_than_or_equal_to(1)) }
     it { is_expected.to(validate_presence_of(:status)) }
+    it { is_expected.to(validate_presence_of(:email)) }
   end
 end
