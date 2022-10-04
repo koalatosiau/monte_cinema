15b35fcd15367ecefc6c36298a169fa107f7d791
Alicja Szynka <alicja.szynka@monterail.com>
2022-10-04 10:23:37 +0200

    Update emails migration with CR suggestions

--- db/migrate/20221003172937_add_email_to_reservations.rb
+++ db/migrate/20221003172937_add_email_to_reservations.rb
@@ -1,12 +1,8 @@
 class AddEmailToReservations < ActiveRecord::Migration[7.0]
   class Reservation < ActiveRecord::Base
-    end
 
     def change
-      add_column :reservations, :email, :string
-      Reservation.reset_column_information
-      Reservation.all.each do |reservation|
-        reservation.update(:email => 'example@example.com'  )
+      add_column :reservations, :email, :string, null: false
       end
     end
 end
