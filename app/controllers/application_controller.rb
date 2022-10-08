56e13ff3f995b7586de53de9411fdbda771b02cc
Alicja Szynka <alicja.szynka@monterail.com>
2022-10-08 17:58:12 +0200

    Update existing user flow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
-    redirect_back(fallback_location: root_path)
+    redirect_to root_path
   end
 end
